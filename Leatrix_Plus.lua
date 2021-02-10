afd7524ac203f17ba68bda62458fda6ecbc6530e
leatrix <58888415+leatrix@users.noreply.github.com>
2021-02-10 18:04:07 +0000

    9.0.16.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.0.15 (9th February 2021)
+-- 	Leatrix Plus 9.0.16.alpha.1 (10th February 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.0.15"
+	LeaPlusLC["AddonVer"] = "9.0.16.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -5627,6 +5627,7 @@
 		if LeaPlusLC["LockoutSharing"] == "On" then
 			-- Check the display menu option, update the game options panel and lockout changes
 			ShowAccountAchievements(true)
+			InterfaceOptionsSocialPanelShowAccountAchievments:SetChecked(true)
 			InterfaceOptionsPanel_CheckButton_Update(InterfaceOptionsSocialPanelShowAccountAchievments)
 			InterfaceOptionsSocialPanelShowAccountAchievments:Disable()
 			InterfaceOptionsSocialPanelShowAccountAchievments:SetAlpha(0.5)
