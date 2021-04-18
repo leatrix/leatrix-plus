74690851aa194d65f520f6b44a29c2e1848394ca
leatrix <58888415+leatrix@users.noreply.github.com>
2021-04-18 02:26:42 +0100

    9.0.25.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.0.24 (14th April 2021)
+-- 	Leatrix Plus 9.0.25.alpha.1 (18th April 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.0.24"
+	LeaPlusLC["AddonVer"] = "9.0.25.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -5416,13 +5416,11 @@
 						-- Handle colors
 						if r and g and b and chatTypeID then
 							local colorCode = RGBToColorCode(r, g, b)
-							chatMessage = string.gsub(chatMessage, "|r", "|r" .. colorCode) -- Links
 							chatMessage = colorCode .. chatMessage
 						end
 
 						chatMessage = gsub(chatMessage, "|T.-|t", "") -- Remove textures
-						chatMessage = gsub(chatMessage, "{.-}", "") -- Remove ellipsis
-						editBox:Insert(chatMessage .. "|n")
+						editBox:Insert(chatMessage .. "|r|n")
 
 					end
 					totalMsgCount = totalMsgCount + 1
