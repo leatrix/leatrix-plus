db9e844a65d44eae01aabb8497918f34c382dd27
leatrix <58888415+leatrix@users.noreply.github.com>
2021-04-18 16:25:33 +0100

    9.0.25.alpha.4

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.0.25.alpha.3 (18th April 2021)
+-- 	Leatrix Plus 9.0.25.alpha.4 (18th April 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.0.25.alpha.3"
+	LeaPlusLC["AddonVer"] = "9.0.25.alpha.4"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -5440,6 +5440,7 @@
 							chatMessage = colorCode .. chatMessage
 						end
 
+						chatMessage = gsub(chatMessage, "|T.-|t", "") -- Remove textures
 						editBox:Insert(chatMessage .. "|r|n")
 
 					end
