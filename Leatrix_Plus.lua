8700dbe1b619b0a2c8b457277bbfc1e507ae65e3
leatrix <58888415+leatrix@users.noreply.github.com>
2021-11-16 08:45:22 +0000

    9.1.23.alpha.16

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.1.23.alpha.15 (15th November 2021)
+-- 	Leatrix Plus 9.1.23.alpha.16 (16th November 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.1.23.alpha.15"
+	LeaPlusLC["AddonVer"] = "9.1.23.alpha.16"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -385,6 +385,7 @@
 --	Set lock state for configuration buttons
 	function LeaPlusLC:SetDim()
 		LeaPlusLC:LockOption("AutomateQuests", "AutomateQuestsBtn", false)			-- Automate quests
+		LeaPlusLC:LockOption("AutoAcceptRes", "AutoAcceptResBtn", false)			-- Accept resurrection
 		LeaPlusLC:LockOption("AutoReleasePvP", "AutoReleasePvPBtn", false)			-- Release in PvP
 		LeaPlusLC:LockOption("AutoSellJunk", "AutoSellJunkBtn", false)				-- Sell junk automatically
 		LeaPlusLC:LockOption("AutoRepairGear", "AutoRepairBtn", false)				-- Repair automatically
@@ -567,16 +568,6 @@
 			LpEvt:RegisterEvent("PARTY_INVITE_REQUEST");
 		else
 			LpEvt:UnregisterEvent("PARTY_INVITE_REQUEST");
-		end
-
-		----------------------------------------------------------------------
-		--	Accept resurrection
-		----------------------------------------------------------------------
-
-		if LeaPlusLC["AutoAcceptRes"] == "On" then
-			LpEvt:RegisterEvent("RESURRECT_REQUEST");
-		else
-			LpEvt:UnregisterEvent("RESURRECT_REQUEST");
 		end
 
 		----------------------------------------------------------------------
@@ -4506,6 +4497,111 @@
 ----------------------------------------------------------------------
 
 	function LeaPlusLC:Player()
+
+		----------------------------------------------------------------------
+		-- Automatically accept resurrection requests (no reload required)
+		----------------------------------------------------------------------
+
+		do
+
+			-- Create configuration panel
+			local AcceptResPanel = LeaPlusLC:CreatePanel("Accept resurrection", "AcceptResPanel")
+
+			LeaPlusLC:MakeTx(AcceptResPanel, "Settings", 16, -72)
+			LeaPlusLC:MakeCB(AcceptResPanel, "AutoResNoCombat", "Exclude combat resurrection", 16, -92, false, "If checked, resurrection requests will not be automatically accepted if the player resurrecting you is in combat.")
+
+			-- Help button hidden
+			AcceptResPanel.h:Hide()
+
+			-- Back button handler
+			AcceptResPanel.b:SetScript("OnClick", function() 
+				AcceptResPanel:Hide(); LeaPlusLC["PageF"]:Show(); LeaPlusLC["Page1"]:Show();
+				return
+			end)
+
+			-- Reset button handler
+			AcceptResPanel.r:SetScript("OnClick", function()
+
+				-- Reset checkboxes
+				LeaPlusLC["AutoResNoCombat"] = "On"
+
+				-- Refresh panel
+				AcceptResPanel:Hide(); AcceptResPanel:Show()
+
+			end)
+
+			-- Show panal when options panel button is clicked
+			LeaPlusCB["AutoAcceptResBtn"]:SetScript("OnClick", function()
+				if IsShiftKeyDown() and IsControlKeyDown() then
+					-- Preset profile
+					LeaPlusLC["AutoResNoCombat"] = "On"
+				else
+					AcceptResPanel:Show()
+					LeaPlusLC:HideFrames()
+				end
+			end)
+
+			-- Function to set resurrect event
+			local function SetResEvent()
+				if LeaPlusLC["AutoAcceptRes"] == "On" then
+					AcceptResPanel:RegisterEvent("RESURRECT_REQUEST")
+				else
+					AcceptResPanel:UnregisterEvent("RESURRECT_REQUEST")
+				end
+			end
+
+			-- Run function when option is clicked and on startup if option is enabled
+			LeaPlusCB["AutoAcceptRes"]:HookScript("OnClick", SetResEvent)
+			if LeaPlusLC["AutoAcceptRes"] == "On" then SetResEvent() end
+
+			-- Handle event
+			AcceptResPanel:SetScript("OnEvent", function(self, event, arg1)
+				if event == "RESURRECT_REQUEST" then
+
+					-- Exclude pylon and brazier requests
+					local pylonLoc
+
+					-- Exclude Failure Detection Pylon
+					pylonLoc = "Failure Detection Pylon"
+					if 	   GameLocale == "zhCN" then pylonLoc = "故障检测晶塔"
+					elseif GameLocale == "zhTW" then pylonLoc = "滅團偵測水晶塔"
+					elseif GameLocale == "ruRU" then pylonLoc = "Пилон для обнаружения проблем"
+					elseif GameLocale == "koKR" then pylonLoc = "고장 감지 변환기"
+					elseif GameLocale == "esMX" then pylonLoc = "Pilón detector de errores"
+					elseif GameLocale == "ptBR" then pylonLoc = "Pilar Detector de Falhas"
+					elseif GameLocale == "deDE" then pylonLoc = "Fehlschlagdetektorpylon"
+					elseif GameLocale == "esES" then pylonLoc = "Pilón detector de errores"
+					elseif GameLocale == "frFR" then pylonLoc = "Pylône de détection des échecs"
+					elseif GameLocale == "itIT" then pylonLoc = "Pilone d'Individuazione Fallimenti"
+					end
+					if arg1 == pylonLoc then return	end
+
+					-- Exclude Brazier of Awakening
+					pylonLoc = "Brazier of Awakening"
+					if 	   GameLocale == "zhCN" then pylonLoc = "觉醒火盆"
+					elseif GameLocale == "zhTW" then pylonLoc = "覺醒火盆"
+					elseif GameLocale == "ruRU" then pylonLoc = "Жаровня пробуждения"
+					elseif GameLocale == "koKR" then pylonLoc = "각성의 화로"
+					elseif GameLocale == "esMX" then pylonLoc = "Blandón del Despertar"
+					elseif GameLocale == "ptBR" then pylonLoc = "Braseiro do Despertar"
+					elseif GameLocale == "deDE" then pylonLoc = "Kohlenbecken des Erwachens"
+					elseif GameLocale == "esES" then pylonLoc = "Blandón de Despertar"
+					elseif GameLocale == "frFR" then pylonLoc = "Brasero de l'Éveil"
+					elseif GameLocale == "itIT" then pylonLoc = "Braciere del Risveglio"
+					end
+					if arg1 == pylonLoc then return	end
+
+					-- Manage other resurrection requests
+					if not UnitAffectingCombat(arg1) or LeaPlusLC["AutoResNoCombat"] == "Off" then
+						AcceptResurrect()
+						StaticPopup_Hide("RESURRECT_NO_TIMER")
+					end
+					return
+
+				end
+			end)
+
+		end
 
 		----------------------------------------------------------------------
 		-- Easy mount special
@@ -9738,54 +9834,6 @@
 		end
 
 		----------------------------------------------------------------------
-		-- Automatically accept resurrection requests
-		----------------------------------------------------------------------
-
-		if event == "RESURRECT_REQUEST" then
-
-			-- Exclude pylon and brazier requests
-			local pylonLoc
-
-			-- Exclude Failure Detection Pylon
-			pylonLoc = "Failure Detection Pylon"
-			if 	   GameLocale == "zhCN" then pylonLoc = "故障检测晶塔"
-			elseif GameLocale == "zhTW" then pylonLoc = "滅團偵測水晶塔"
-			elseif GameLocale == "ruRU" then pylonLoc = "Пилон для обнаружения проблем"
-			elseif GameLocale == "koKR" then pylonLoc = "고장 감지 변환기"
-			elseif GameLocale == "esMX" then pylonLoc = "Pilón detector de errores"
-			elseif GameLocale == "ptBR" then pylonLoc = "Pilar Detector de Falhas"
-			elseif GameLocale == "deDE" then pylonLoc = "Fehlschlagdetektorpylon"
-			elseif GameLocale == "esES" then pylonLoc = "Pilón detector de errores"
-			elseif GameLocale == "frFR" then pylonLoc = "Pylône de détection des échecs"
-			elseif GameLocale == "itIT" then pylonLoc = "Pilone d'Individuazione Fallimenti"
-			end
-			if arg1 == pylonLoc then return	end
-
-			-- Exclude Brazier of Awakening
-			pylonLoc = "Brazier of Awakening"
-			if 	   GameLocale == "zhCN" then pylonLoc = "觉醒火盆"
-			elseif GameLocale == "zhTW" then pylonLoc = "覺醒火盆"
-			elseif GameLocale == "ruRU" then pylonLoc = "Жаровня пробуждения"
-			elseif GameLocale == "koKR" then pylonLoc = "각성의 화로"
-			elseif GameLocale == "esMX" then pylonLoc = "Blandón del Despertar"
-			elseif GameLocale == "ptBR" then pylonLoc = "Braseiro do Despertar"
-			elseif GameLocale == "deDE" then pylonLoc = "Kohlenbecken des Erwachens"
-			elseif GameLocale == "esES" then pylonLoc = "Blandón de Despertar"
-			elseif GameLocale == "frFR" then pylonLoc = "Brasero de l'Éveil"
-			elseif GameLocale == "itIT" then pylonLoc = "Braciere del Risveglio"
-			end
-			if arg1 == pylonLoc then return	end
-
-			-- Manage other resurrection requests
-			if not UnitAffectingCombat(arg1) then
-				AcceptResurrect()
-				StaticPopup_Hide("RESURRECT_NO_TIMER")
-			end
-			return
-
-		end
-
-		----------------------------------------------------------------------
 		-- Accept summon
 		----------------------------------------------------------------------
 
@@ -9926,6 +9974,7 @@
 				LeaPlusLC:LoadVarChk("AutomateGossip", "Off")				-- Automate gossip
 				LeaPlusLC:LoadVarChk("AutoAcceptSummon", "Off")				-- Accept summon
 				LeaPlusLC:LoadVarChk("AutoAcceptRes", "Off")				-- Accept resurrection
+				LeaPlusLC:LoadVarChk("AutoResNoCombat", "On")				-- Accept resurrection exclude combat
 				LeaPlusLC:LoadVarChk("AutoReleasePvP", "Off")				-- Release in PvP
 				LeaPlusLC:LoadVarChk("AutoReleaseNoAlterac", "Off")			-- Release in PvP Exclude Alterac Valley
 				LeaPlusLC:LoadVarChk("AutoReleaseNoWintergsp", "Off")		-- Release in PvP Exclude Wintergrasp
@@ -10152,6 +10201,7 @@
 			LeaPlusDB["AutomateGossip"]			= LeaPlusLC["AutomateGossip"]
 			LeaPlusDB["AutoAcceptSummon"] 		= LeaPlusLC["AutoAcceptSummon"]
 			LeaPlusDB["AutoAcceptRes"] 			= LeaPlusLC["AutoAcceptRes"]
+			LeaPlusDB["AutoResNoCombat"] 		= LeaPlusLC["AutoResNoCombat"]
 			LeaPlusDB["AutoReleasePvP"] 		= LeaPlusLC["AutoReleasePvP"]
 			LeaPlusDB["AutoReleaseNoAlterac"] 	= LeaPlusLC["AutoReleaseNoAlterac"]
 			LeaPlusDB["AutoReleaseNoWintergsp"] = LeaPlusLC["AutoReleaseNoWintergsp"]
@@ -12658,7 +12708,7 @@
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutomateQuests"			,	"Automate quests"				,	146, -92, 	false,	"If checked, quests will be selected, accepted and turned-in automatically.|n|nQuests which have a gold, currency or crafting reagent requirement will not be turned-in automatically.|n|nYou can hold the shift key down when you talk to a quest giver to override this setting.")
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutomateGossip"			,	"Automate gossip"				,	146, -112, 	false,	"If checked, you can hold down the alt key while opening a gossip window to automatically select a single gossip option.")
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutoAcceptSummon"			,	"Accept summon"					, 	146, -132, 	false,	"If checked, summon requests will be accepted automatically unless you are in combat.")
-	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutoAcceptRes"				,	"Accept resurrection"			, 	146, -152, 	false,	"If checked, resurrection requests will be accepted automatically as long as the player resurrecting you is not in combat.|n|nResurrection requests from a Brazier of Awakening or a Failure Detection Pylon will not be accepted automatically.")
+	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutoAcceptRes"				,	"Accept resurrection"			, 	146, -152, 	false,	"If checked, resurrection requests will be accepted automatically.|n|nResurrection requests from a Brazier of Awakening or a Failure Detection Pylon will not be accepted automatically.")
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutoReleasePvP"			,	"Release in PvP"				, 	146, -172, 	false,	"If checked, you will release automatically after you die in Ashran, Tol Barad (PvP), Wintergrasp or any battleground.|n|nYou will not release automatically if you have the ability to self-resurrect (soulstone, reincarnation, etc).")
 
 	LeaPlusLC:MakeTx(LeaPlusLC[pg], "Vendors"					, 	340, -72)
@@ -12666,6 +12716,7 @@
 	LeaPlusLC:MakeCB(LeaPlusLC[pg], "AutoRepairGear"			, 	"Repair automatically"			,	340, -112, 	false,	"If checked, your gear will be repaired automatically when you visit a suitable merchant.|n|nYou can hold the shift key down when you talk to a merchant to override this setting.")
 
  	LeaPlusLC:CfgBtn("AutomateQuestsBtn", LeaPlusCB["AutomateQuests"])
+	LeaPlusLC:CfgBtn("AutoAcceptResBtn", LeaPlusCB["AutoAcceptRes"])
 	LeaPlusLC:CfgBtn("AutoReleasePvPBtn", LeaPlusCB["AutoReleasePvP"])
  	LeaPlusLC:CfgBtn("AutoSellJunkBtn", LeaPlusCB["AutoSellJunk"])
  	LeaPlusLC:CfgBtn("AutoRepairBtn", LeaPlusCB["AutoRepairGear"])
