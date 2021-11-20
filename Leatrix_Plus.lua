b3741698ba218841026b9041fe60f0d0f73ff66e
leatrix <58888415+leatrix@users.noreply.github.com>
2021-11-20 10:50:04 +0000

    9.1.24.alpha.3

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.1.24.alpha.2 (19th November 2021)
+-- 	Leatrix Plus 9.1.24.alpha.3 (20th November 2021)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.1.24.alpha.2"
+	LeaPlusLC["AddonVer"] = "9.1.24.alpha.3"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -2763,6 +2763,11 @@
 			hooksecurefunc(EventToastManagerFrame, "Show", function()
 				if not EventToastManagerFrame.HideButton:IsShown() then
 					if EventToastManagerFrame.currentDisplayingToast then
+						if IsInJailersTower() then
+							-- Show floor summary
+							local title = EventToastManagerFrame.currentDisplayingToast.Title:GetText() or nil
+							if title and strfind(title, JAILERS_TOWER_SCENARIO_FLOOR) and strfind(title, COMPLETE) then return end
+						end
 						EventToastManagerFrame.currentDisplayingToast:OnAnimatedOut()
 					end
 				end
