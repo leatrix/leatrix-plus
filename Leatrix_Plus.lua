e3c34ced56218dcab447020a289afcf9dbd4bcb1
leatrix <58888415+leatrix@users.noreply.github.com>
2022-01-19 14:28:11 +0000

    9.1.39

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.1.39.alpha.4 (17th January 2022)
+-- 	Leatrix Plus 9.1.39 (19th January 2022)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.1.39.alpha.4"
+	LeaPlusLC["AddonVer"] = "9.1.39"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
