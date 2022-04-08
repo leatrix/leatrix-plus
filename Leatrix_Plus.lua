7f672f84efe2aa03f594c2ce7f7f1b16533d4132
leatrix <58888415+leatrix@users.noreply.github.com>
2022-04-08 01:44:29 +0100

    9.2.06.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 9.2.05 (6th April 2022)
+-- 	Leatrix Plus 9.2.06.alpha.1 (8th April 2022)
 ----------------------------------------------------------------------
 
 --	01:Functions	20:Live			50:RunOnce		70:Logout			
@@ -20,7 +20,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "9.2.05"
+	LeaPlusLC["AddonVer"] = "9.2.06.alpha.1"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
@@ -36,6 +36,14 @@
 			end)
 			return
 		end
+	end
+
+	-- Check for incompatible addons
+	if IsAddOnLoaded("NDui") then
+		C_Timer.After(5, function()
+			print("Leatrix Plus is not compatible with NDui.  To use Leatrix Plus, you need to uninstall NDui.")
+		end)
+		return
 	end
 
 ----------------------------------------------------------------------
