3beaa0269f1a14a146b44d12518f05f0e23d3bb0
leatrix <58888415+leatrix@users.noreply.github.com>
2023-03-23 21:44:05 +0000

    10.0.52

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 10.0.51 (18th March 2023)
+-- 	Leatrix Plus 10.0.52 (23rd March 2023)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "10.0.51"
+	LeaPlusLC["AddonVer"] = "10.0.52"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
--- Leatrix_Plus_Media.lua
+++ Leatrix_Plus_Media.lua
@@ -643,6 +643,7 @@
 		"|Cffffffff" .. L["Dragonflight Legacies (3)"] .. " |r#4854572#350", -- interface/cinematics/dragonflight_100_dt.mp3
 		"|Cffffffff" .. L["The Ebon Scales"] .. " |r#4687624#23", -- interface/cinematics/shadowlands/dragonflight_1001_didc.mp3
 		"|Cffffffff" .. L["The Seed of Hope"] .. " |r#4687619#128", -- unknown
+		"|Cffffffff" .. L["Secrets of the Reach"] .. " |r#4925533#231", -- unknown
 	})
 
 	Zn(L["Various"], L["Various"], L["Class Trials"]							, {	"|cffffd800" .. L["Various"] .. ": " .. L["Class Trials"], prefol, "MUS_70_ClassTrial_Horde_BattleWalk#71954", "MUS_70_ClassTrial_Alliance_BattleWalk#71959",})
@@ -785,6 +786,7 @@
 		L["Dragonflight Legacies (3)"] .. " |r(968)",
 		L["The Ebon Scales"] .. " |r(969)",
 		L["The Seed of Hope"] .. " |r(963)",
+		L["Secrets of the Reach"] .. " |r(974)",
 	})
 
 	----------------------------------------------------------------------
