8562a34a835333965c05ba1567d640d5465d839b
leatrix <58888415+leatrix@users.noreply.github.com>
2023-08-23 09:27:01 +0100

    10.1.17

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 10.1.16 (15th August 2023)
+-- 	Leatrix Plus 10.1.17 (23rd August 2023)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "10.1.16"
+	LeaPlusLC["AddonVer"] = "10.1.17"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
