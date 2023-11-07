287754000815bd90ef293b380c8ee05c4c1e3107
leatrix <58888415+leatrix@users.noreply.github.com>
2023-11-07 04:53:12 +0000

    10.1.29.alpha.1

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -8548,6 +8548,9 @@
 				-- Nameplate tooltip
 				if NamePlateTooltip then NamePlateTooltip:SetScale(LeaPlusLC["LeaPlusTipSize"]) end
 
+				-- Game settings panel tooltip
+				if SettingsTooltip then SettingsTooltip:SetScale(LeaPlusLC["LeaPlusTipSize"]) end
+
 				-- LibDBIcon
 				if LibDBIconTooltip then LibDBIconTooltip:SetScale(LeaPlusLC["LeaPlusTipSize"]) end
 
