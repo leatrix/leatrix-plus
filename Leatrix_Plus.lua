ec6315cd3e95909cb628983fae15546a76677b9d
leatrix <support@leatrix.com>
2024-01-16 10:34:09 +0000

    10.2.10

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 10.2.10.alpha.14 (10th January 2024)
+-- 	Leatrix Plus 10.2.10 (16th January 2024)
 ----------------------------------------------------------------------
 
 --	01:Functns, 02:Locks, 03:Restart, 20:Live, 30:Isolated, 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "10.2.10.alpha.14"
+	LeaPlusLC["AddonVer"] = "10.2.10"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
