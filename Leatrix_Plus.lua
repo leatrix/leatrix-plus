b9773ab2a7604a2e8afccc82561e26d7930a6f4d
leatrix <support@leatrix.com>
2024-11-27 12:20:53 +0000

    11.0.19

--- Leatrix_Plus.lua
+++ Leatrix_Plus.lua
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------
--- 	Leatrix Plus 11.0.18 (20th November 2024)
+-- 	Leatrix Plus 11.0.19 (27th November 2024)
 ----------------------------------------------------------------------
 
 --	01:Functions 02:Locks,  03:Restart 40:Player
@@ -18,7 +18,7 @@
 	local void
 
 	-- Version
-	LeaPlusLC["AddonVer"] = "11.0.18"
+	LeaPlusLC["AddonVer"] = "11.0.19"
 
 	-- Get locale table
 	local void, Leatrix_Plus = ...
